e983089179c879da5bcb2945c036b8ebfde52ccf
Rado Jedro <radoslaw.jedro@gmail.com>
2019-07-20 20:31:37 +0100

    Remove additional space script.js file

--- js/script.js
+++ js/script.js
@@ -11,16 +11,13 @@ else if (randomNumber==2) {
 else if (randomNumber==3) {
     computerMove = 'nozyce';
 }
-
 else {computerMove = 'Cos poszlo nie tak';
 }
 console.log('Ruch komputera to: ' + computerMove);
 
-
 //playerMove and playerInput
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
 console.log('Gracz wpisał: ' + playerInput);
 
 let playerMove = playerInput;

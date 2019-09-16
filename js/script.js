a2c1c91654ab76ff0bc5352ad7acf4b55209e0e0
Rado Jedro <radoslaw.jedro@gmail.com>
2019-09-16 11:44:45 +0100

    Remove old, unused code

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ function playGame(playerInput){
   const scissors = 'nozyce';
   //computerMove
   let randomNumber = Math.floor(Math.random() * 3 + 1);
-  //let computerMove = randomNumber;
 
   let computerMove = getMoveName(randomNumber);
 
@@ -29,15 +28,10 @@ function playGame(playerInput){
       return 'nieznany ruch';
     }
   }
-
   console.log('computerMove to ' + computerMove);
 
   argComputerMove=computerMove;
 
-  //let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-  //printMessage('Gracz wpisał: ' + playerInput);
-
-  //let playerMove = playerInput;
   let playerMove = getMoveName(playerInput);
   argPlayerMove=playerMove;
 
@@ -80,7 +74,6 @@ function playGame(playerInput){
 document.getElementById('play-rock').addEventListener('click', function(){
   playGame (1);
 });
-
 document.getElementById('play-paper').addEventListener('click', function(){
   playGame(2);
 });
@@ -89,71 +82,4 @@ document.getElementById('play-scissors').addEventListener('click', function(){
 });
 
 }
-
-//OLD TASKS
-
-
-/*if(randomNumber == 1){
-  computerMove = rock;
-}
-else if (randomNumber==2) {
-    computerMove = paper;
-}
-else if (randomNumber==3) {
-    computerMove = scissors;
-}
-else {computerMove = 'Cos poszlo nie tak';
-}
-*/
-//printMessage('Ruch komputera to: ' + computerMove);
-
-//playerMove and playerInput
-/*
-if(playerInput == '1'){
-  playerMove = rock;
-}
-else if(playerInput == '2'){
-  playerMove = paper;
-}
-else if(playerInput == '3'){
-  playerMove = scissors;
-}
-  else {playerMove = 'Nieprawidlowy wybor !';
-}
-*/
-/*
-printMessage('Twój ruch to: ' + playerMove);
-
-
-if( computerMove == rock && playerMove == paper){
-  printMessage('Ty wygrywasz!');
-}
-else if( computerMove == rock && playerMove == scissors){
-  printMessage('Ty przegrywasz!');
-}
-else if( computerMove == rock && playerMove == rock){
-  printMessage('Remis! Gramy jeszcze raz');
-}
-else if( computerMove == paper && playerMove == paper){
-  printMessage('Remis! Gramy jeszcze raz');
-}
-else if( computerMove == paper && playerMove == scissors){
-  printMessage('Ty wygrywasz!');
-}
-else if( computerMove == paper && playerMove == rock){
-  printMessage('Ty przegrywasz!');
-}
-else if( computerMove == scissors && playerMove == paper){
-  printMessage('Ty przegrywasz!');
-}
-else if( computerMove == scissors && playerMove == rock){
-  printMessage('Ty wygrywasz!');
-}
-else if( computerMove == scissors && playerMove == scissors){
-  printMessage('Remis! Gramy jeszcze raz');
-}
-else printMessage('Fatal error');
-
-*/
-
 
